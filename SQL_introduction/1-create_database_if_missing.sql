0ef791fcb9ce94e91cded3eac62c7f84af3465c7
Bonginkosi <gbongig@gmail.com>
2023-08-19 19:23:28 +0200

    Create a database

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,6 +1,3 @@
 -- Script that creates a data baseif data base exists sripts should not fail
 
-IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'htbn_0c_0')
-BEGIN
-    CREATE DATABASE htbn_0c_0;
-END;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
