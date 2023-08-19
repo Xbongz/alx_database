69aeff7cc96f233fa4eeb3c13d9a435543b979c1
Bonginkosi <gbongig@gmail.com>
2023-08-19 11:04:34 +0200

    Create a database

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,3 @@
--- Script that creates a data base
--- if data base exists
--- sripts should not fail
-CREATE DATABASE htbn_0c_0
+-- Script that creates a data baseif data base exists sripts should not fail
+
+CREATE DATABASE [IF NOT EXISTS] htbn_0c_0:
